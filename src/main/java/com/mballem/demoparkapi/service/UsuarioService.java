a6011da7ce12ff519ae2053c4fb3bec36fcca497
antoniojunio <juniordsj@gmail.com>
2024-06-29 15:03:05 -0300

    adicionado tratamento de exceções

--- src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
@@ -1,8 +1,11 @@
 package com.mballem.demoparkapi.service;
 
 import com.mballem.demoparkapi.entity.Usuario;
+import com.mballem.demoparkapi.exception.EntityNotFoundException;
+import com.mballem.demoparkapi.exception.UsernameUniqueViolationException;
 import com.mballem.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,13 +19,17 @@ public class UsuarioService {
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
-        return usuarioRepository.save(usuario);
+        try {
+            return usuarioRepository.save(usuario);
+        } catch (DataIntegrityViolationException ex) {
+            throw new UsernameUniqueViolationException(String.format("Username '%s' já cadastrado", usuario.getUsername()));
+        }
     }
 
     @Transactional(readOnly = true)
     public Usuario buscarPorId(Long id) {
         return usuarioRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Usuário não encontrado.")
+                () -> new EntityNotFoundException(String.format("Usuário id=%s não encontrado", id))
         );
     }
 
@@ -32,13 +39,13 @@ public class UsuarioService {
             throw new RuntimeException("Nova senha não confere com confirmação de senha.");
         }
 
-        Usuario usuer = buscarPorId(id);
-        if (!usuer.getPassword().equals(senhaAtual)) {
+        Usuario usuario = buscarPorId(id);
+        if (!usuario.getPassword().equals(senhaAtual)) {
             throw new RuntimeException("Sua senha não confere.");
         }
 
-        usuer.setPassword(novaSenha);
-        return usuer;
+        usuario.setPassword(novaSenha);
+        return usuario;
     }
 
     @Transactional(readOnly = true)
